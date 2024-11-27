199c0908dc2e4d80df87889da939a83ceab7fd1e
Veterstift <71975430+Veterstift@users.noreply.github.com>
2024-11-27 11:37:40 +0100

    Folders en meer

--- index.php
+++ index.php
@@ -5,7 +5,40 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
-<body>
+<style>
     
+</style>
+<body>
+    <div class="container">
+        <div class="navbar">
+            <div class="logo_div">
+                <a href="ìndex.php"><h1>Placeholder</h1></a>
+            </div>
+            <ul>
+                <li><a class="active" href="index.php">Home</a></li>
+                <li><a href="#">placeholder2</a></li>
+                <li><a href="#">placeholder2</a></li>
+                <li><a href="#">placeholder2</a></li>
+            </ul>
+        </div>
+        <!-- // navbar -->
+
+        <!-- page content -->
+        <div class="content">
+            <h2 class="content-title">test</h2>
+            <hr>
+            <!-- content -->
+        </div>
+        <!-- // page content -->
+
+        <!-- footer -->
+        <div class="footer">
+            <p>&copy; 2020 Placeholder</p>
+        </div>
+        <!-- // footer -->
+        
+    </div>
+    <!-- // container -->
+
 </body>
 </html>
